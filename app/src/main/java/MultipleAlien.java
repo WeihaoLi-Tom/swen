b272c5a70851c10ec86343e02ffa1e83326a8d65
WeihaoLi-Tom <weihao4@student.unimelb.edu.au>
2023-09-07 16:50:48 +1000

    multi_fix

--- app/src/main/java/MultipleAlien.java
+++ app/src/main/java/MultipleAlien.java
@@ -1,25 +1,32 @@
-//public class MultipleAlien extends Alien {
-//    private Alien[][] alienGrid;
-//    private boolean hasTransformed = false;
-//
-//    public MultipleAlien(String imageName, String type, int rowIndex, int colIndex, Alien[][] grid) {
-//        super(imageName, type, rowIndex, colIndex);
-//        this.alienGrid = grid;
-//    }
-//
-//    @Override
-//    public void act() {
-//        super.act();
-//
-//        if (!hasTransformed && Math.random() < 0.01 && getRowIndex() > 0) { // 1% chance to transform
-//            for (int col = 0; col < alienGrid[0].length; col++) {
-//                Alien newAlien = new Alien("sprites/alien.gif", "alien", getRowIndex() - 1, col);
-//                alienGrid[getRowIndex() - 1][col] = newAlien;
-//            }
-//            setType("alien");
-//            hasTransformed = true;
-//        }
-//    }
-//}
+import ch.aplu.jgamegrid.Location;
+public class MultipleAlien extends Alien {
+    private Alien[][] alienGrid;
+    private boolean hasTransformed = false;
+
+    public MultipleAlien(String imageName, String type, int rowIndex, int colIndex, Alien[][] grid) {
+        super(imageName, type, rowIndex, colIndex);
+        this.alienGrid = grid;
+    }
+
+    @Override
+    public void act() {
+        super.act();
+
+        if (!hasTransformed && Math.random() < 1 && getRowIndex() > 0) { // 1% chance to transform
+            SpaceInvader game = (SpaceInvader) getGameGrid();
+            int nbCols = alienGrid[0].length; // 获取列数
+            for (int col = 0; col < nbCols; col++) {
+                Alien newAlien = new Alien("sprites/alien.gif", "alien", getRowIndex() - 1, col);
+                alienGrid[getRowIndex() - 1][col] = newAlien;
+                Location currentLocation = getLocation(); // 获取当前位置
+                game.addActor(newAlien, new Location(currentLocation.x + (col - this.getColIndex()) * 10, currentLocation.y - 10)); // 根据当前位置和列的差异来计算新的位置
+            }
+            setType("alien");
+            hasTransformed = true;
+        }
+
+
+    }
+}
 
 
