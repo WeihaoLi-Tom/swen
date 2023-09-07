044cb3bb9c9476eefeece709f15ad3e63d8939cd
WeihaoLi-Tom <weihao4@student.unimelb.edu.au>
2023-09-07 17:08:05 +1000

    multi_fix

--- app/src/main/java/MultipleAlien.java
+++ app/src/main/java/MultipleAlien.java
@@ -19,7 +19,7 @@ public class MultipleAlien extends Alien {
                 Alien newAlien = new Alien("sprites/alien.gif", "alien", getRowIndex() - 1, col);
                 alienGrid[getRowIndex() - 1][col] = newAlien;
                 Location currentLocation = getLocation(); // 获取当前位置
-                game.addActor(newAlien, new Location(currentLocation.x + (col - this.getColIndex()) * 10, currentLocation.y - 10)); // 根据当前位置和列的差异来计算新的位置
+                game.addActor(newAlien, new Location(currentLocation.x + (col - this.getColIndex()) * 10, currentLocation.y - 30)); // 根据当前位置和列的差异来计算新的位置
             }
             setType("alien");
             hasTransformed = true;
